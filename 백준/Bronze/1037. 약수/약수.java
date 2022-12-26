9d8c1504a998f8a778be63c83e77d884e4fbf348
sonchangwoo <96860725+5onchangwoo@users.noreply.github.com>
2022-12-26 18:24:54 +0900

    [Bronze I] Title: 약수, Time: 132 ms, Memory: 14668 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int result = Arrays.stream(nums).min().getAsInt() * Arrays.stream(nums).max().getAsInt();
+        System.out.println(result);
+    }
+}
