24bb3fbad3add6d0af4311451e26e880037d0c24
SON CHANGWOO <96860725+5onchangwoo@users.noreply.github.com>
2022-12-31 16:31:21 +0900

    [Silver I] Title: 카잉 달력, Time: 224 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.java
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -1,4 +1,3 @@
-import javax.xml.stream.FactoryConfigurationError;
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -7,7 +6,7 @@ public class Main {
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine().trim());
+        int t = Integer.parseInt(br.readLine().trim());
         // 입력
         /*
          * 각 테스트 데이터는 한 줄로 구성된다.
@@ -15,40 +14,29 @@ public class Main {
          * (1 ≤ M, N ≤ 40,000, 1 ≤ x ≤ M, 1 ≤ y ≤ N)
          * 여기서 <M:N>은 카잉 달력의 마지막 해를 나타낸다.
          *  */
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < t; i++) {
             String line = br.readLine();
             StringTokenizer st = new StringTokenizer(line);
-            int M = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            int gcd = gcd(Math.max(M, N), Math.min(M, N));
-            int lcm = (M * N / gcd);
-            // x가 올수있는 년도만 확인하면 범위를 줄일 수 있다.
-            // date = M*i + x;
-            boolean isbreak = false;
-            for (int date = x; date <= lcm; date += M) {
-                if ((date - 1) % N + 1 == y) {
-                    bw.append(String.valueOf(date)).append("\n");
-                    isbreak = true;
-                    break;
-                }
-            }
-            if (!isbreak) {
-                bw.append("-1").append("\n");
-            }
+            bw.append(String.valueOf(calcDate(m, n, x, y))).append("\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static int gcd(int m, int n) {
-        if (m % n == 0) {
-            return n;
+    private static int calcDate(int m, int n, int x, int y) throws IOException {
+        // x가 올수있는 년도만 확인하면 범위를 줄일 수 있다.
+        // date = M*i + x;
+        for (int date = x; date <= m * n; date += m) {
+            if ((date - 1) % n + 1 == y) {
+                return date;
+            }
         }
-        return gcd(n, m % n);
+        return -1;
     }
-
 
 }
