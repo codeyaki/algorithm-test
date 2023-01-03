330a0cc2349a580dfe653e8894a33314e8dc0547
SON CHANGWOO <96860725+5onchangwoo@users.noreply.github.com>
2023-01-03 14:33:46 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    private static int n, m;
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static List<Integer> numList = new ArrayList<>();
+    private static boolean[] visitedList = new boolean[8 + 1];
+    public static void main(String[] args) throws IOException {
+        int[] numList = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        n = numList[0];
+        m = numList[1];
+        dfs(0, 0);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void dfs(int point, int length) throws IOException {
+
+        if (length >= m ) {
+            for (Integer num : numList) {
+                bw.append(String.valueOf(num)).append(" ");
+            }
+            bw.append("\n");
+            return;
+        }
+        for (int i = point; i < n ; i++) {
+            if (visitedList[i]) {
+                continue;
+            }
+            visitedList[i] = true;
+            numList.add(i + 1);
+            dfs(i + 1, length + 1);
+            numList.remove(numList.size() - 1);
+            visitedList[i] = false;
+        }
+
+    }
+}
