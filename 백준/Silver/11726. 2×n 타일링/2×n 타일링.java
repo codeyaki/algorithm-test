bb83b60055a037d7de94cd4b6cb62756d8570ce9
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-01-28 15:52:14 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int MAX = 1000 + 1;
+    static int[] cache = new int[MAX + 1];
+
+    public static void main(String[] args) throws IOException {
+        dp();
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(cache[num]);
+    }
+
+    private static void dp() {
+        cache[1] = 1;
+        cache[2] = 2;
+        for (int i = 3; i < MAX; i++) {
+            cache[i] = (cache[i - 2] + cache[i - 1]) % 10007;
+        }
+    }
+
+}
