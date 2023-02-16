5a07aa49b79d8854ae180a01e6be1c48a42362b8
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-16 23:08:54 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int[] common) {
-        int degree = common[1] - common[0];
-        boolean isDif = true;
-        for(int i =2; i<common.length; i++){
-            int tmp = common[i] - common[i-1];
-            if(degree != tmp) {
-                degree = common[i]/common[i-1];
-                isDif = false;
-                break;
-            }
-            break;
+        int degree1 = common[1] - common[0];
+        int degree2 = common[2] - common[1];
+        if(degree1 == degree2){
+            return common[common.length - 1] + degree1;
+        } else{
+            return common[common.length - 1] * (common[2]/common[1]);
         }
-        return isDif ? common[common.length -1] + degree :  common[common.length -1] * degree;
     }
 }
