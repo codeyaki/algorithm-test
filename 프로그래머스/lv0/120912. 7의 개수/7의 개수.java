339cb6ee2c0872b8747226b586b51797205579bd
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-17 19:15:13 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int[] array) {
-        StringBuilder sb = new StringBuilder();
         int answer = 0;
-        for (int i = 0; i < array.length; i++) {
-            sb.append(array[i]);
+        for(int number : array){
+            while (number > 1){
+                if(number%10 == 7) answer++;
+                number /= 10;
+            }
         }
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == '7') answer++;
-        }
-
         return answer;
     }
 }
