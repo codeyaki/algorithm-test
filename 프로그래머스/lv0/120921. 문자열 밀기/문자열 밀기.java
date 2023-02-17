933bd8c88971c729d416b9302d578ec1ae28844d
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-17 17:48:27 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        String loopA = A;
-        while (answer < A.length()){
-            if(loopA.equals(B)) return answer;
-            StringBuffer newSbA = new StringBuffer();
-            newSbA.append(loopA.charAt(loopA.length() - 1));
-            for (int i = 0; i < A.length() - 1; i++) {
-                newSbA.append(loopA.charAt(i));
-            }
-            loopA = newSbA.toString();
-            answer++;
-        }
-        return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
