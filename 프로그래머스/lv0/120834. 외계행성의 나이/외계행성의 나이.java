48e241498412c566cd01156e456530193047788a
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 19:27:16 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -2,9 +2,9 @@ class Solution {
     public String solution(int age) {
         StringBuilder sb = new StringBuilder();
         while(age>0){
-            sb.append((char) ('a' + age % 10));
+            sb.insert(0, (char) ('a' + age % 10));
             age /= 10;
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
