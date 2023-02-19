6b754fba12920275a756b7faf6be29004ab15183
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 16:18:20 +0900

    [level 0] Title: 순서쌍의 개수, Time: 10.50 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,9 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0) answer++;
-        }
-        return answer;
+        return (int) IntStream.range(1, n + 1).filter(i -> n % i == 0).count();
     }
 }
