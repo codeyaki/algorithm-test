4c41d1a2220fd166ae32c23ffb1a6563b8990ca4
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 19:56:39 +0900

    [level 0] Title: 숫자 찾기, Time: 7.42 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = String.valueOf(num).indexOf(String.valueOf(k));
-        return answer == -1 ? -1 : answer + 1;
+        return (" " + num).indexOf(String.valueOf(k));
     }
 }
