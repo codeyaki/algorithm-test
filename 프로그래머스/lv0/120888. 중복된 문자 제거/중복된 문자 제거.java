35fdd49981f6ddf67e82a609a1c2cc836ed40210
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 20:11:56 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.88 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,16 +1,10 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string) {
-        Set<Character> set = new HashSet<>();
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if(set.contains(c)) continue;
-            set.add(c);
-            answer.append(c);
-        }
-        return answer.toString();
+        return my_string.chars()
+                .mapToObj(Character::toString)
+                .distinct()
+                .collect(Collectors.joining());
     }
 }
