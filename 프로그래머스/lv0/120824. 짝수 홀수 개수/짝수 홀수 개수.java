336768f40a77945fba96d4aa62c76a2b4e11d3c9
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 14:55:17 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 1.36 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -3,10 +3,7 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] cnt = {0, 0};
-        Arrays.stream(num_list).forEach(n -> {
-            if (n % 2 == 0) cnt[0]++;
-            else cnt[1]++;
-        });
+        Arrays.stream(num_list).forEach(n -> cnt[n % 2]++);
         return cnt;
     }
 }
