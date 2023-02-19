5d7e8accebd897d9cb89addf97b0d0815267a4b8
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 14:45:06 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        int length = my_string.length();
-        for(int i =0; i<length; i++){
-            sb.append(my_string.charAt(length - i - 1));
-        }
-        return sb.toString();
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.reverse().toString();
     }
 }
