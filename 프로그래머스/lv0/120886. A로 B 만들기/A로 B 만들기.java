c59d8a7166cae596d76ba57918f90d5005f320d3
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-19 21:36:27 +0900

    [level 0] Title: A로 B 만들기, Time: 0.47 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,18 +1,11 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
 
 class Solution {
     public int solution(String before, String after) {
-        String[] beforeSplit = before.split("");
-        String[] afterSplit = after.split("");
-        Arrays.sort(beforeSplit);
-        Arrays.sort(afterSplit);
-        for (int i = 0; i < beforeSplit.length; i++) {
-            if (!beforeSplit[i].equals(afterSplit[i])) return 0;
-        }
-
-        return 1;
+        char[] beforeChars = before.toCharArray();
+        char[] afterChars = after.toCharArray();
+        Arrays.sort(beforeChars);
+        Arrays.sort(afterChars);
+        return new String(beforeChars).equals(new String(afterChars)) ? 1 : 0;
     }
 }
