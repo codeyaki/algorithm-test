7e222a1e2c95f39da466c2c4b2bddd89ae4f58b6
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-20 15:27:16 +0900

    [level 0] Title: k의 개수, Time: 60.59 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        String str = "";
-        for(int a = i; a <= j; a++) {
-            str += a+"";
+        int answer = 0;
+        for(int num = i; num <= j; num++){
+            String stringNum = String.valueOf(num);
+            String afterNum = stringNum.replaceAll(String.valueOf(k), "");
+            answer += stringNum.length() - afterNum.length();
         }
-
-        return str.length() - str.replace(k+"", "").length();
+        return answer;
     }
 }
