cc7dbc78fe578dd4567e8ef1eaaa9017ece89cc8
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-21 11:09:16 +0900

    [level 0] Title: 특이한 정렬, Time: 3.46 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,27 +1,18 @@
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
-    public Integer[] solution(int[] numlist, int n) {
-        List<Integer> nList = Arrays.stream(numlist).boxed().collect(Collectors.toList());
-        nList.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                int dis1 = Math.abs(o1 - n);
-                int dis2 = Math.abs(o2 - n);
+    public int[] solution(int[] numlist, int n) {
+        return Arrays.stream(numlist).boxed().sorted((o1, o2) -> {
+            int dis1 = Math.abs(o1 - n);
+            int dis2 = Math.abs(o2 - n);
 
-                if (dis1 == dis2) {
-                    return o2 - o1;
-                }
-                return dis1 - dis2;
+            if (dis1 == dis2) {
+                return o2 - o1;
             }
-        });
-        Integer[] answer = nList.toArray(Integer[]::new);
+            return dis1 - dis2;
 
+        }).mapToInt(Integer::intValue).toArray();
 
-        return answer;
     }
 
 }
