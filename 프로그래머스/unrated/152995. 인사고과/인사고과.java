f852a35c85b354972b6ca0670c015a54d5254975
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-02-24 18:46:08 +0900

    [unrated] Title: 인사고과, Time: 74.25 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.java
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.java
@@ -1,31 +1,22 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        int answer = 1;
-        int n = scores.length;
-        int[] inho = scores[0];
-        // 인호가 인센티브를 받을 수 있는지 확인
-        if (!check(scores[0], scores)) return -1;
-        // 인호보다 높은 성적을 낸 사람중 인센티브를 못받는 사람 확인
-        for (int i = 1; i < n; i++) {
-            int[] now = scores[i];
-            // 인호보다 성적이 좋은 경우
-            if (now[0] + now[1] > inho[0] + inho[1]) {
-                // 인센티브를 받을 수 없는 사람인 경우 넘어가기
-                if (!check(scores[i], scores)) continue;
-                answer++;
+        int[] s = scores[0];
+        Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
+        ArrayList<Integer> arr = new ArrayList<>();
+        int max = 0, ans = 1;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
+            }
+            else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
             }
         }
-        return answer;
-    }
-
-    boolean check(int[] target, int[][] scores){
-        for(int[] member : scores){
-            if (target[0] < member[0] && target[1] < member[1]) {
-                return false;
-            }
-        }
-        return true;
+        return ans;
     }
 }
