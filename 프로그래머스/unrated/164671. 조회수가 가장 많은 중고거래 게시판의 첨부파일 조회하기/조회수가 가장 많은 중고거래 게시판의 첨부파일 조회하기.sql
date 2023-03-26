70a89f242ba77e85bb3ee5332ee4c04e20e4d9b5
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-03-26 14:08:18 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,14 +1,10 @@
 -- 코드를 입력하세요
-SELECT '/home/grep/src/' || B.BOARD_ID || '/' || B.FILE_ID || B.FILE_NAME || B.FILE_EXT as "FILE_PATH"
-  FROM USED_GOODS_FILE B
-  JOIN (SELECT A.BOARD_ID
-          FROM (SELECT *
-                  FROM USED_GOODS_BOARD C
-                 ORDER BY C.VIEWS DESC
-               ) A
-         WHERE ROWNUM = 1
-       ) D ON (B.BOARD_ID = D.BOARD_ID)
- ORDER BY B.FILE_ID DESC
-   
-  
-
+SELECT '/home/grep/src/' || A.BOARD_ID || '/' || A.FILE_ID || A.FILE_NAME || A.FILE_EXT as "FILE_PATH"
+  FROM USED_GOODS_FILE A
+  JOIN (SELECT BOARD_ID
+          FROM USED_GOODS_BOARD INNER
+         WHERE INNER.VIEWS = (SELECT MAX(VIEWS)
+                                FROM USED_GOODS_BOARD
+                             )
+       ) B ON(A.BOARD_ID = B.BOARD_ID)
+ ORDER BY A.FILE_ID DESC
