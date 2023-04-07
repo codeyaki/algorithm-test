d3e6f64dfe56db32a4bae44899c2ea8293aad6f6
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-04-07 19:23:43 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.12 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -5,20 +5,20 @@ class Solution {
         long total = 0;
         int left = 0;
         List<int[]> list = new LinkedList<>();
-        int[] answer = new int[2];
+        int[] answer = new int[]{0, sequence.length};
         for(int right = 0; right < sequence.length; right++){
             total += sequence[right];
             // 만약 seqeunce[i]를 더했을때 7이 넘으면 left + 1
             while(total > k && left < right){
                 total -= sequence[left++];
             }
-            // k와 같다면 stop
+            // k와 같다면 정답이 될 수 있는지 비교
             if(total == k){
-                list.add(new int[]{left, right});
+                if((answer[1] - answer[0]) > (right - left)){
+                    answer = new int[]{left, right};
+                }
             }
         }
-        Collections.sort(list, (o1, o2) -> (o1[1] - o1[0]) - (o2[1] - o2[0]));
-        answer = list.get(0);
         return answer;
     }
 }
