6d17cd2a0bcd1c8b1e38f1c54eb4de308b74683a
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-04-18 17:49:02 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,38 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int ingre : ingredient){
-            stack.push(ingre);
-            while(isBurger(stack)){
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
                 answer++;
             }
         }
         return answer;
-    }
-    public boolean isBurger(Stack<Integer> stack){
-        if(stack.size() < 4) return false;
-        // stack이므로 반대로 맞춰야함 
-        int[] rightIngre = new int[]{1, 3, 2, 1};
-        Stack<Integer> tmp = new Stack<>();
-        boolean result = true;
-        // 맨위의 4개를 꺼내서 햄버거가 될 수 있는지 확인
-        for(int i = 0; i < 4; i++){
-            int ingre = stack.pop();
-            tmp.push(ingre);
-            if(ingre != rightIngre[i]){
-                result = false;
-                break;
-            }
-        }
-        // 만약 햄버거가 될 수 없다면 다시 저장해야함
-        if(!result){
-            while(!tmp.isEmpty()){
-                stack.push(tmp.pop());
-            }
-        }
-        return result;
     }
 }
