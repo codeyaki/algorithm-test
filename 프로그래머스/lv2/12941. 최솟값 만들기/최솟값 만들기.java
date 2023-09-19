b313056a60135c763c78c4c5dc9983cebe126e60
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-09-19 10:49:04 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.77 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,17 +3,12 @@ import java.util.*;
 class Solution{
     public int solution(int[] A, int[] B){
         int n = A.length;
-        Integer[] boxedA = new Integer[n];
-        Integer[] boxedB = new Integer[n];
-        for(int i = 0; i < n; i++){
-            boxedA[i] = A[i];
-            boxedB[i] = B[i];
-        }
-        Arrays.sort(boxedA);
-        Arrays.sort(boxedB, Collections.reverseOrder());
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
         int answer = 0;
-        for(int i = 0; i < A.length; i++){
-            answer += boxedA[i] * boxedB[i];
+        for(int i = 0; i < n; i++){
+            answer += A[i] * B[n - i - 1];
         }
 
         return answer;
