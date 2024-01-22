f0a4c4c8fb2afc8ae8e9dc2950f583b729c948ff
codeyaki <scw9410@gmail.com>
2024-01-22 10:55:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.21 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        StringBuilder sb = new StringBuilder(s);
         for(int i = 0; i < s.length(); i++){
-            StringBuilder sb = new StringBuilder(s);
-            String sub = sb.substring(0, i);
-            sb.delete(0, i);
+            String sub = sb.substring(0, 1);
+            sb.delete(0, 1);
             sb.append(sub);
             if(checkString(sb.toString())) answer++;
         }
