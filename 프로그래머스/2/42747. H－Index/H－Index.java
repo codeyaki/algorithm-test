33a34af371e817d392f3d972bd8023d75f3e014a
codeyaki <scw9410@gmail.com>
2024-01-26 11:05:37 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,14 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        for(int h = 0; h < 10000 + 1; h++) {
-            int underH = 0;
-            int upperH = 0;
-            for(int i = 0; i < citations.length; i++){
-                if(citations[i] >= h) upperH++;
-                if(citations[i] <= h) underH++;
-            }
-            if(underH <= h && upperH >= h) answer = h;
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
+            answer = Math.max(answer, smaller);
         }
         return answer;
     }
