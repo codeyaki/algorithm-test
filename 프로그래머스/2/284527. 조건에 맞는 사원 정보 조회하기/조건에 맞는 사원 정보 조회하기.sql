806e31a2d6b305ba123bd75974f63d4968048af6
codeyaki <scw9410@gmail.com>
2024-03-11 10:42:00 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,8 +1,15 @@
+with HGF as (
+    select sum(HG.SCORE) as SCORE
+         , HG.EMP_NO
+      from HR_GRADE HG
+     group by HG.EMP_NO, HG.YEAR
+     order by SCORE DESC
+     limit 1
+)
+
+
 -- 코드를 작성해주세요
-select sum(HG.SCORE) as SCORE, HG.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
+select HGF.SCORE, HE.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
   from HR_DEPARTMENT HD
   join HR_EMPLOYEES HE on (HD.DEPT_ID = HE.DEPT_ID)
-  join HR_GRADE HG on (HE.EMP_NO = HG.EMP_NO)
- group by HG.EMP_NO, HG.YEAR
- order by SCORE DESC
- limit 1
+  join HGF on (HE.EMP_NO = HGF.EMP_NO)
