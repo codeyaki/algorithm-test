d7b47292b8b693ffab8db88ff18f3a2880a73d0e
codeyaki <scw9410@gmail.com>
2024-03-25 18:10:08 +0900

    [level 2] Title: 가장 큰 수, Time: 4.37 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,13 +1,12 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        // 모든 원소가 0인 경우
-        if(Arrays.stream(numbers).sum() == 0) return "0"; 
         Integer[] result = Arrays.stream(numbers).boxed().sorted((o1, o2) -> {
             int num1 = Integer.parseInt(o1 + "" + o2);
             int num2 = Integer.parseInt(o2 + "" + o1);
             return num2 - num1;
         }).toArray(Integer[]::new);
+        if(result[0] == 0) return "0";
         return convertString(result);
     }
     
