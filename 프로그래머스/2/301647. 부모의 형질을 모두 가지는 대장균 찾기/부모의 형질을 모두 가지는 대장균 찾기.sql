8f6f1142c8e470fc48edd14f506bbfcd35efa86b
codeyaki <scw9410@gmail.com>
2024-05-27 18:06:25 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select child.ID
+     , child.GENOTYPE
+     , parent.GENOTYPE as "PARENT_GENOTYPE"
+  from ECOLI_DATA child
+  join ECOLI_DATA parent on child.PARENT_ID = parent.ID
+ where child.GENOTYPE & parent.GENOTYPE = parent.GENOTYPE
+ order by child.ID asc
