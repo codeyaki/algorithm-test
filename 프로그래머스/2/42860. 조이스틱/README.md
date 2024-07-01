9ad94bde84db7ef482473629727334e132ee7282
codeyaki <scw9410@gmail.com>
2024-07-01 17:35:54 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 01일 17:35:11
+2024년 07월 01일 17:35:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(String name) {
-        char[] nameCharArr = name.toCharArray();
-        int n = nameCharArr.length;
+        // 1. 각 자리별 상하 변경 횟수 구하기
         int answer = 0;
-        int move = n - 1;
-        for(int start = 0; start < n; start++) {
-            //1. 'A'가 아니면 알파벳 조작
-            if(nameCharArr[start] != 'A') answer += getUpAndDown(nameCharArr[start]);
-            //2.최소횟수 구하기
-            int end = start + 1;
-            while(end < n && nameCharArr[end] == 'A') end++;
-            int temp = Math.min(2 * start + n - end, start + 2 * (n - end));
-            move = Math.min(move, temp);            
+        for(char c : name.toCharArray()) {
+            answer += getUpAndDown(c);
         }
-        answer += move;
+        System.out.println(answer);
+        // 2. 전체 좌우 횟수 구하기
+        answer += getLeftAndRight(name);
         return answer;
     }
     
     private int getUpAndDown(char target) {
         return Math.min(target - 'A', 'Z' - target + 1);
     }
+    
+    private int getLeftAndRight(String name) {
+        int result = 0;
+        return result;
+    }
+    
     
 }
